ad739e421a4a37456edf79db0aead56b46cca91c
abc354402600 <130091560+abc354402600@users.noreply.github.com>
2025-02-18 20:40:02 +0800

    Add files via upload

---
+++ CPP/01_第一章_数据类型、运算符/11_字符型数据类型/README.md
@@ -0,0 +1,3 @@
+## 1.char类型本质上是数字，通过ASCII表进行的映射
+	
+## 2.char可以存储的范围是超过ASCII的，但是基于ASCII映射，可以认为char应用内容就是ASCII表
---
+++ CPP/01_第一章_数据类型、运算符/11_字符型数据类型/字符型数据类型.cpp
@@ -0,0 +1,21 @@
+// Created by 黑马程序员.
+#include "iostream"
+#include "windows.h"
+using namespace std;
+
+
+int main()
+{
+    SetConsoleOutputCP(CP_UTF8);
+
+    char ch = 65;
+    cout << ch << endl;
+
+    char ch2 = 'a';
+    cout << ch2 + 1 << endl;
+
+    char ch3 = 'a' + 2;
+    cout << ch3 << endl;
+
+    return 0;
+}
